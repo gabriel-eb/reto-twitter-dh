72268c74014feb07be25c8e69a271088a5cf3602
daviders98 <davider98@hotmail.com>
2021-08-12 18:53:41 -0600

    added post

--- routes/main.js
+++ routes/main.js
@@ -10,6 +10,7 @@ const perfilMiddleware = require('../middlewares/perfilMiddleware');
 
 router.get("/index", controller.index);
 router.get("/index2", controller.index2);
+router.post("/index2",controller.sendTweet);
 router.get("/edit", controller.edit);
 router.get("/login", controller.login);
 router.get("/signup", controller.signup);
