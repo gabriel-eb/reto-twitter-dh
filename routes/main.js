836847c82f03d6a113d0c6080e67a112989aa136
oscarfedu <oscarfd@live.com>
2021-08-12 19:37:57 -0500

    creando la ruta main y agregando el ejs index2.ejs

---
+++ routes/main.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const controller = require("../controllers/mainController");
+const {body} = require('express-validator');
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const perfilMiddleware = require('../middlewares/perfilMiddleware');
+
+
+router.get("/index", controller.index);
+router.get("/index2", controller.index2);
+router.get("/edit", controller.edit);
+router.get("/login", controller.login);
+router.get("/signup", controller.signup);
+
+module.exports = router;
